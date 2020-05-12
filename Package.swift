0dc4f451492c388d84720ca8b8247ac4a66f0d94
ChenPengfei <afeiafeia@163.com>
2020-05-12 11:16:27 +0800

    内部依赖表现异常，都放在同一个文件夹下

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CPFUIKit",
-            targets: ["CPFUIKit", "CustomizableButton"]),
+            targets: ["CPFUIKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,12 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CPFUIKit",
-            dependencies: ["CustomizableButton"]),
-        .target(
-            name: "CustomizableButton",
             dependencies: []),
         .testTarget(
             name: "CPFUIKitTests",
-            dependencies: ["CustomizableButton"]),
+            dependencies: ["CPFUIKit"]),
     ]
 )
--- Sources/CPFUIKit/CPFUIKit.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Aaron on 2020/5/12.
-//
-
-import Foundation
-import CustomizableButton
