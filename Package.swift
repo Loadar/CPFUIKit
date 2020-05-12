506b458b3cb91da190c03d18ceedefdbb238c4eb
ChenPengfei <afeiafeia@163.com>
2020-05-12 10:14:55 +0800

    建立一个汇总的target

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CPFUIKit",
-            targets: ["CustomizableButton"]),
+            targets: ["CPFUIKit", "CustomizableButton"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,6 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CPFUIKit",
+            dependencies: ["CustomizableButton"]),
         .target(
             name: "CustomizableButton",
             dependencies: []),
---
+++ Sources/CPFUIKit/CPFUIKit.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aaron on 2020/5/12.
+//
+
+import Foundation
+import CustomizableButton
